642d53839d54383356a69aabf85e1a3718f66808
Mohammad Kiani <60611017+mkiani12@users.noreply.github.com>
2024-09-22 00:09:41 +0330

    fixed

--- pages/index.vue
+++ pages/index.vue
@@ -5,11 +5,13 @@
   <v-row>
     <v-col cols="12">
       <v-row>
-        <v-col cols="12"> </v-col>
+        <v-col cols="12">
+          <DashboardLinkCards />
+        </v-col>
         <!-- Sales overview -->
-        <v-col cols="12" lg="8"> </v-col>
+        <v-col cols="12" lg="8"> <DashboardProcess /> </v-col>
         <!-- Yearly Breakup / Monthly Earnings -->
-        <v-col cols="12" lg="4"> </v-col>
+        <v-col cols="12" lg="4"><DashboardRequests /> </v-col>
       </v-row>
     </v-col>
   </v-row>
