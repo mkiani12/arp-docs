39decbb929f415d0d1d3a5448b25ea46ea8dac84
Mohammad Kiani <60611017+mkiani12@users.noreply.github.com>
2024-09-22 00:21:36 +0330

    ok

--- pages/docs/backend.vue
+++ pages/docs/backend.vue
@@ -4,5 +4,17 @@ useHead({
 });
 </script>
 <template>
-  <div></div>
+  <v-container class="pa-0">
+    <v-card>
+      <v-card-title>Test</v-card-title>
+      <v-card-text
+        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe hic,
+        quae doloribus similique natus sint quidem perspiciatis in perferendis
+        eius! Necessitatibus est ipsa quaerat, excepturi aspernatur beatae quod
+        praesentium eaque hic facere. Recusandae id voluptate quidem? Eligendi,
+        quos aperiam minima inventore, voluptatum facilis placeat tenetur error
+        aspernatur explicabo, illum distinctio!
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
